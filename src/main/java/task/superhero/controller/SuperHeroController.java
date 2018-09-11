26609963b53b6c4fd9b04cedb096eee4e4e3e186
Yelko <yelko@MacBook-Pro-de-Yelko.local>
2018-09-11 13:09:14 +0200

    add logs

--- src/main/java/task/superhero/controller/SuperHeroController.java
+++ src/main/java/task/superhero/controller/SuperHeroController.java
@@ -4,6 +4,8 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -22,27 +24,37 @@ import task.superhero.service.SuperHeroService;
 @RequestMapping("/api")
 public class SuperHeroController {
 
+    Logger logger = LoggerFactory.getLogger(SuperHeroController.class);
+
     @Autowired
     private SuperHeroService superHeroService;
 
     @GetMapping("/superhero")
     public @ResponseBody List<SuperHero> getAllSuperHeros(){
+        logger.debug("Accessing list of all superheroes");
         return superHeroService.getAllSuperHeros();
     }
 
     @GetMapping("/superhero/{id}")
     public ResponseEntity<Object> getSuperHero(@PathVariable Long id){
         Optional<SuperHero> superHero = superHeroService.findSuperHeroById(id);
-
-        return superHero.isPresent() ? ResponseEntity.ok().body(superHero.get()) : ResponseEntity.notFound().build();
+        if (superHero.isPresent()){
+            logger.debug("Accessing superhero {}",superHero.get().getId());
+            return ResponseEntity.ok().body(superHero.get());
+        } else {
+            logger.error("Accessing non existing superhero {}",superHero.get().getId());
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping("/superhero/add")
     public ResponseEntity<CreationFeedBack> addSuperHero(@Valid SuperHero newSuperHero, BindingResult bindingResult){
         if(!bindingResult.hasErrors()){
             superHeroService.addSuperHero(newSuperHero);
+            logger.info("New superhero added with id = {}", newSuperHero.getId());
             return ResponseEntity.ok(new CreationFeedBack(true,bindingResult.getAllErrors(),newSuperHero));
         }
+        logger.error("Error trying to create superhero: {}", bindingResult.getAllErrors());
         return ResponseEntity.badRequest().body(new CreationFeedBack(false, bindingResult.getAllErrors(),newSuperHero));
 
     }
